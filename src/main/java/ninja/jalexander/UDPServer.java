eefa03b3cab038f3519741c0e05b2578a73a9ef5
Sierra <sierrasch@gmail.com>
2018-02-26 23:14:19 -0800

    Splits up the byte array in UDPServer into UDP sized chunks

--- src/main/java/ninja/jalexander/UDPServer.java
+++ src/main/java/ninja/jalexander/UDPServer.java
@@ -2,11 +2,30 @@ package main.java.ninja.jalexander;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 public class UDPServer extends Thread {
     public void run() {
+
         byte[] myByteArray = Util.loadImageFile();
         System.out.println("UDP Server Running with file of size " + myByteArray.length);
+
+        int chunkSize = 60000;
+        int chunkCount = (myByteArray.length / chunkSize) + 1;
+	//System.out.println("Chunk Count is: " + chunkCount);
+	byte[][] bytesChunked = new byte[chunkCount][];
+	
+	for (int i = 0; i < chunkCount - 1; i++) {
+	    bytesChunked[i] = new byte[chunkSize];
+	    bytesChunked[i] = Arrays.copyOfRange(myByteArray, i * chunkSize, (i + 1) * chunkSize);
+	    //System.out.println("Other one: " + bytesChunked[i].length);
+	}
+	//do last one
+	int lastChunkSize = myByteArray.length - ((chunkCount - 2) * chunkSize);
+	bytesChunked[chunkCount - 1] = new byte[lastChunkSize];
+	bytesChunked[chunkCount - 1] = Arrays.copyOfRange(myByteArray, (chunkCount - 1) * chunkSize, myByteArray.length);
+	//System.out.println("Last one: " + bytesChunked[chunkCount -1].length);
+	/*
 
         try(DatagramSocket serverSocket = new DatagramSocket(9876)){
             byte[] receiveData = new byte[1024];
@@ -27,6 +46,6 @@ public class UDPServer extends Thread {
         } catch (Exception e) {
             System.err.println(e.getMessage());
             e.printStackTrace();
-        }
+	    }*/
     }
 }
