844d0bb31a830d0edf477ed5422e65449e7b7265
John Alexander <jalexander.qed@gmail.com>
2018-03-12 18:11:25 -0700

    Add print to ping server.

--- src/main/java/ninja/jalexander/PingServer.java
+++ src/main/java/ninja/jalexander/PingServer.java
@@ -5,23 +5,26 @@ import java.net.*;
 
 public class PingServer extends Thread {
     public void run() {
-	try (DatagramSocket serverSocket = new DatagramSocket(5678)) {
-		while (true) {
-		    byte[] receiveData = new byte[10];
-		    byte[] sendData = new byte[1];
-		    String ret = "b";
-		    sendData = ret.getBytes();
-		    while(true) {
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-			serverSocket.receive(receivePacket);
-			DatagramPacket sendPacket =
-			    new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
-			serverSocket.send(sendPacket);
-		    }
-		}
-	    }catch (IOException e) {
-	    System.err.println(e.getMessage());
-	    e.printStackTrace();
-	}
+        try (DatagramSocket serverSocket = new DatagramSocket(5678)) {
+            while (true) {
+                byte[] receiveData = new byte[10];
+                byte[] sendData = new byte[1];
+                String ret = "b";
+                sendData = ret.getBytes();
+                while (true) {
+                    DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                    serverSocket.receive(receivePacket);
+
+                    System.out.println("Received ping request");
+
+                    DatagramPacket sendPacket =
+                            new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+                    serverSocket.send(sendPacket);
+                }
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
